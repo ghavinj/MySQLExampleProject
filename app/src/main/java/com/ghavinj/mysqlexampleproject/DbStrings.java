856a01d6f2750be0e3c7224b8bd13cc6e68213be
GhavinJ <ghavinj@icloud.com>
2019-05-27 00:09:24 -0400

    added button

---
+++ app/src/main/java/com/ghavinj/mysqlexampleproject/DbStrings.java
@@ -0,0 +1,9 @@
+package com.ghavinj.mysqlexampleproject;
+
+public class DbStrings {
+
+    static final String DATABASE_URL = "192.168.0.1:3306";
+    static final String DATABASE_NAME = "db_exampl";
+    static final String USERNAME = "some_user";
+    static final String PASSWORD = "your_password ",
+}
